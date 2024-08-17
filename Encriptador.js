6a719f452e0c591d849a21a81e4228cb31da5499
NotKenny14 <104523418+NotKenny14@users.noreply.github.com>
2024-08-17 09:31:25 -0600

    Ok

---
+++ Encriptador.js
@@ -0,0 +1,91 @@
+function encriptar(traduccion) {
+    document.querySelector("#warning").removeAttribute("style");
+    var textarea = document.querySelector("#data"); // Ajuste del ID
+    const texto = textarea.value;
+    var area_default = document.querySelector("#data");
+    var area_result = document.querySelector("#texto_out");
+    
+    if (texto != "") {
+        var out = "";
+        for (var i = 0; i < texto.length; i++) {
+            if (((texto[i] < 'a') || (texto[i] > 'z')) && (texto[i] != ' ')) {
+                document.querySelector("#warning").style.color = "red";
+                document.querySelector("#warning").style.fontSize = "16px";
+                return;
+            } else if ((texto.length == 1 && texto == " ") || texto.replace(/ /g, "") == "") {
+                area_default.classList.remove("invisible");
+                area_result.classList.add("invisible");
+                return;
+            }
+
+            if (texto[i] == 'a') {
+                out += traduccion["a"];
+            } else if (texto[i] == 'e') {
+                out += traduccion["e"];
+            } else if (texto[i] == 'i') {
+                out += traduccion["i"];
+            } else if (texto[i] == 'o') {
+                out += traduccion["o"];
+            } else if (texto[i] == 'u') {
+                out += traduccion["u"];
+            } else {
+                out += texto[i];
+            }
+        }
+
+        area_default.classList.add("invisible");
+        area_result.classList.remove("invisible");
+        area_result.value = out; // Actualiza el valor del área de texto
+    }
+
+    return;
+}
+
+function desencriptar(traduccion) {
+    document.querySelector("#warning").removeAttribute("style");
+    var textarea = document.querySelector("#data"); // Ajuste del ID
+    var texto = textarea.value;
+    var area_default = document.querySelector("#data");
+    var area_result = document.querySelector("#texto_out");
+
+    if (texto != "") {
+        for (var i = 0; i < texto.length; i++) {
+            if (((texto[i] < 'a') || (texto[i] > 'z')) && (texto[i] != ' ')) {
+                document.querySelector("#warning").style.color = "red";
+                document.querySelector("#warning").style.fontSize = "16px";
+                return;
+            } else if ((texto.length == 1 && texto == " ") || texto.replace(/ /g, "") == "") {
+                area_default.classList.remove("invisible");
+                area_result.classList.add("invisible");
+                return;
+            }
+        }
+        area_default.classList.add("invisible");
+        area_result.classList.remove("invisible");
+        texto = texto.replace(new RegExp(traduccion["a"], "g"), "a");
+        texto = texto.replace(new RegExp(traduccion["e"], "g"), "e");
+        texto = texto.replace(new RegExp(traduccion["i"], "g"), "i");
+        texto = texto.replace(new RegExp(traduccion["o"], "g"), "o");
+        texto = texto.replace(new RegExp(traduccion["u"], "g"), "u");
+        area_result.value = texto; 
+    }
+
+    return;
+}
+
+function clipboard() {
+    const texto_out = document.querySelector("#texto_out");
+    navigator.clipboard.writeText(texto_out.value);
+}
+
+const enc = document.querySelector('#enc');
+const des = document.querySelector('#des');
+const copy = document.querySelector('#copiar');
+
+var traduccion = {"a": "ai", "e": "enter", "i": "imes", "o": "ober", "u": "ufat"};
+
+enc.addEventListener('click', function () { encriptar(traduccion); });
+des.addEventListener('click', function () { desencriptar(traduccion); });
+copy.addEventListener('click', function () { clipboard(); });
+
+
